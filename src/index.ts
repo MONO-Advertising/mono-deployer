1ab024f92d75dfc33c41919c38d7b88b16a4c32c
Kla Haeck <kla@zsh.email>
2025-05-01 22:35:19 -0500

    logs

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { pushToEnvironment } from './lib/push-to-env';
 const app = express();
 const port = process.env.PORT || 3000;
 const deployKey = process.env.DEPLOYMENT_KEY;
-const vercelDeployWebhook = process.env.VERCEL_DEPLOY_WEBHOOK!;
+const vercelDeployWebhook = process.env.VERCEL_DEPLOY_WEBHOOK;
 
 app.use(express.json());
 
@@ -20,16 +20,20 @@ app.post('/api/deploy', (req: Request, res: Response): any => {
     return res.status(401).json({ error: 'Unauthorized' });
   }
   pushToEnvironment().then(() => {
-    fetch(vercelDeployWebhook, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      // body: JSON.stringify({
-      //   deploymentKey,
-      //   pageId,
-      // }),
-    });
+    console.log('Deployment promise resolved');
+    if (vercelDeployWebhook) {
+      console.log('Triggering Vercel deployment');
+      fetch(vercelDeployWebhook, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // body: JSON.stringify({
+        //   deploymentKey,
+        //   pageId,
+        // }),
+      });
+    }
   }).catch((error) => {
     console.error('Error pushing to environment:', error);
   });
